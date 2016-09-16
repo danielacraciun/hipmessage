2fa2b42618e843ee62d8516ba3a7b510a370cc8a
Daniela Craciun <daniela.craciun@spyhce.com>
2016-09-16 12:25:52 +0300

    generalized implementations of filters and messages

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from hipmessage import BaseFilter, HipMessage, FileBackend
 import json
 
 
-class MessagesWithCards(BaseFilter):
+class PlainMessageFilter(BaseFilter):
+    # Get all messages that are not card
+    without_fields = ['card']
+
+
+class CardMessageFilter(BaseFilter):
     # Get all messages that have a card
     mandatory_fields = ['card']
 
@@ -14,9 +19,9 @@ class MessagesWithCards(BaseFilter):
         return card['style'] == 'link'
 
 
-class CardHipMessages(HipMessage):
+class CardMessage(HipMessage):
     message_backend_class = FileBackend
-    filter_classes = (MessagesWithCards,)
+    filter_classes = (CardMessageFilter,)
 
     def process_message(self, msg):
         card = json.loads(msg['card'])
@@ -24,5 +29,14 @@ class CardHipMessages(HipMessage):
             card['description'].encode('utf8'), card['url'].encode('utf8')))
 
 
-instance = CardHipMessages(token, room_name)
+class PlainMessage(HipMessage):
+    message_backend_class = FileBackend
+    filter_classes = (PlainMessageFilter,)
+
+    def process_message(self, msg):
+        msg = json.loads(msg['card'])
+        print(msg)
+
+
+instance = CardMessage(token, room_name)
 instance.run()
